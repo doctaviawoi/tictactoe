ea982d46d85262b42c1181a433219a01a334e317
Dewi <doct.kwok@gmail.com>
2020-11-13 14:50:58 +0800

    remove app.config['SESSION_FILE_DIR']

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
